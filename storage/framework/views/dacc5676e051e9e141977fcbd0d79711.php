ef2cba92eccd234d609cfc2b2199a3241e6bb7d9
De Jesus Khalil G. <k.dejesus.149420.tc@umindanao.edu.ph>
2026-04-27 00:41:59 +0800

    few changes plus added files in views

---
+++ storage/framework/views/dacc5676e051e9e141977fcbd0d79711.php
@@ -0,0 +1,8 @@
+<section
+    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</section>
+<?php /**PATH C:\Users\parad\dejesus\Boarding_House_Maintenance_And_Management_System\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/section-container.blade.php ENDPATH**/ ?>
