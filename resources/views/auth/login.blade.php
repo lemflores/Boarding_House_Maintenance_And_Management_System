bea01cde4bada45141a8028d10ba90aa215b98a2
DeJesusKhalilG <k.dejesus.149420.tc@umindanao.edu.ph>
2026-05-14 23:19:54 +0800

    icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -79,8 +79,9 @@
                     </label>
                     <div class="flex items-center border border-gray-200 rounded-lg px-3 py-2.5 gap-2 focus-within:border-[#7c3a1e] transition-colors">
                         <span class="text-gray-400 text-[13px] leading-none"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-  <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM1.5 20.25a7.5 7.5 0 0 1 14.99 0A.75.75 0 0 0 16.5 21H3a.75.75 0 0 0 .75-.75Z" clip-rule="evenodd" />
+  <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
 </svg>
+
 </span>
                         <input id="username" type="text" name="username" value="{{ old('username') }}"
                                placeholder="e.g. manager123"
